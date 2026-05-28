2719390c72dbe81fd401a8f1be1e17a999ea0e6a
avni123456 <avnipisharody@gmail.com>
2026-05-28 14:19:34 -0400

    hiiii i did stuff

--- import os.py
+++ import os.py
@@ -14,9 +14,39 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def flip(sprites):
+    return[pygame.transform.flip(sprite, True, False) for sprite in sprites]
+           
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites= []
+        for i in range(sprite_sheet.get_width()//width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+        
+    return all_sprites
+
 
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
+    GRAVITY = 1
+    SPRITES = load_sprite_sheets("main_characters", "pink_man", 32, 32, True)
+    ANIMATION_DELAY = 3
 
     def __init__(self, x, y,width, height):
         self.rect = pygame.Rect(x, y, width, height)
@@ -24,7 +54,8 @@ class Player(pygame.sprite.Sprite):
         self.y_vel = 0
         self.mask = None
         self.direction = "left"
-        self.amimation_count = 0
+        self.animation_count = 0
+        self.fall_count = 0
 
     def move(self, dx, dy):
         self.rect.x += dx
@@ -43,15 +74,34 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
 
     def loop(self, fps):
+        #self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
+
+        self.fall_count += 1
+        self.update_sprite()
     
+    def update_sprite(self):
+        sprite_sheet = "idle" 
+        if self.x_vel !=0:
+            sprite_sheet = "run"
+
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count //self.ANIMATION_DELAY) % len(sprites) 
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+
+    def update(self):
+        self.rect = self.sprite.get_rect(topleft=(self.rect.x, self.rect.y))
+        self.,
+
     def draw(self, win):
-        pygame.draw.rect(win, self.COLOR, self.rect)
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
 def get_background(name):
     #rearrange files and folders in assets then edit code below 
-    image = pygame.image.load(join("assets","brackeys_platformer_assets","sprites", name))
+    image = pygame.image.load(join("assets", "background", name))
     _,_, width, height = image.get_rect()
     tiles = []
 
@@ -82,7 +132,7 @@ def handle_move(player):
 
 def main(window):
     clock = pygame.time.Clock()
-    background, bg_image = get_background("sky.png")
+    background, bg_image = get_background("Blue.png")
 
     player = Player(100, 100, 50, 50)
 
